535695551f6ef0d74b23de62266ab1199b05f3d2
[Nick] <[ageevn007@yandex.ru]>
2023-04-25 22:21:03 +0300

    GSPCh.cpp

---
+++ GSPCh/GSPCh.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <ctime>
+#include <map>
+
+using namespace std;
+int main() {
+    srand(time(0));
+    int sum;
+
+    map <int, int> Cubes;
+    for (int i = 0; i < 1000; i++) 
+    {
+        sum = rand() % 6 + 1 + rand() % 6 + 1; //важно складывать именно два рандома
+        //таким образом у нас получится что кубика два и теория вероятностей сработает
+
+        Cubes[i + 1] = sum;
+    }
+    for (auto elms : Cubes)
+    {
+        cout << "N: " << elms.first << ' ' << "Sum: " << elms.second << endl;
+    }
+}
+
